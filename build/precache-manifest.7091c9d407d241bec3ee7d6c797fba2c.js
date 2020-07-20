6bd1e447e32b964403bfded7e7a15f68494e4a16
ahmedelq <ahmed.elq53@gmail.com>
2020-07-20 10:35:29 +0300

    fixed morgan dependancy

---
+++ build/precache-manifest.7091c9d407d241bec3ee7d6c797fba2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7d21464a972f107edf9cdf58448164",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2edf394f9f1e8703544d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dbaf8825220e57e56d18",
+    "url": "/static/js/2.ad435a02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad435a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2edf394f9f1e8703544d",
+    "url": "/static/js/main.3d2237a1.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
